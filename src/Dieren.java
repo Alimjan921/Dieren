6b8129c0c6cad075376ee5c5cee11272b9a1226e
Alimjan921 <attila0921@hotmail.com>
2020-06-20 19:24:43 +0200

    add Dieren.java

--- src/Dieren.java
+++ src/Dieren.java
@@ -3,6 +3,8 @@ public class Dieren {
     public static void main(String[] args) {
 
         System.out.println("Hello from Gazelle to IT world");
+
+        System.out.println("Hello ebikes");
     }
 }
 
